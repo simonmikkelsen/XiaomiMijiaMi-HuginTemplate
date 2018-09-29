603a01d2193c3c8b9f4fdabc01133e8a4278431b
Simon Mikkelsen <simonmikkelsen@users.noreply.github.com>
2018-09-29 14:19:53 +0200

    Runs with a folder named unstitched.
    Could use real arguments, but that may come later.

--- pythonbatch/processUnstitched.py
+++ pythonbatch/processUnstitched.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python
+#!/usr/bin/python
 
 # Set default logging handler to avoid "No handler found" warnings.
 import glob
@@ -6,17 +6,14 @@ import glob
 import string 
 
 # Here add the path to the default template you want to use...
-<<<<<<< HEAD
-templateString = open('templates/template-reference.pto', 'r').read()
-=======
 templateString = open('templates/template-XiaomiMijiaMi.pto', 'r').read()
->>>>>>> origin/master
 
 # Path of the unstitched images - Copy your JPG files in the "files" folder
-files = glob.glob("files/*.JPG")
+folderName="unstitched"
+files = glob.glob(folderName + "/*.JPG")
 
 for entry in files:
-	filenameImage=entry.replace('files/', '')
+	filenameImage=entry.replace(folderName + '/', '')
 	filenamePTOOutput=entry.replace('.JPG', '.pto')
 
 	stringToWrite=templateString
